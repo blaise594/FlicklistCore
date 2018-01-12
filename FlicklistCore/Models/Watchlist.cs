68777388e6ff49d0dc6279b56c66c34dcf981adf
Daniel <danielbrogers594@gmail.com>
2018-01-12 17:38:16 -0500

    first commit

---
+++ FlicklistCore/Models/Watchlist.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlicklistCore.Models
+{
+    public class Watchlist
+    {
+        public int Id { get; set; }
+        public string Movie { get; set; }
+    }
+    public enum Status
+    {
+        Unwatched,
+        Watched
+    }
+}
---
+++ FlicklistCore/Views/WatchlistsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FlicklistCore.Data;
+using FlicklistCore.Models;
+
+namespace FlicklistCore.Views
+{
+    public class WatchlistsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public WatchlistsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Watchlists
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Watchlist.ToListAsync());
+        }
+
+        // GET: Watchlists/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var watchlist = await _context.Watchlist
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (watchlist == null)
+            {
+                return NotFound();
+            }
+
+            return View(watchlist);
+        }
+
+        // GET: Watchlists/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Watchlists/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Movie")] Watchlist watchlist)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(watchlist);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(watchlist);
+        }
+
+        // GET: Watchlists/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var watchlist = await _context.Watchlist.SingleOrDefaultAsync(m => m.Id == id);
+            if (watchlist == null)
+            {
+                return NotFound();
+            }
+            return View(watchlist);
+        }
+
+        // POST: Watchlists/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Movie")] Watchlist watchlist)
+        {
+            if (id != watchlist.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(watchlist);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!WatchlistExists(watchlist.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(watchlist);
+        }
+
+        // GET: Watchlists/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var watchlist = await _context.Watchlist
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (watchlist == null)
+            {
+                return NotFound();
+            }
+
+            return View(watchlist);
+        }
+
+        // POST: Watchlists/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var watchlist = await _context.Watchlist.SingleOrDefaultAsync(m => m.Id == id);
+            _context.Watchlist.Remove(watchlist);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool WatchlistExists(int id)
+        {
+            return _context.Watchlist.Any(e => e.Id == id);
+        }
+    }
+}
